fb7c5a2f3854ae10627a6bfc2d960433c8e535b4
RicardoRibeiroRodrigues <riywar@gmail.com>
2023-05-17 16:05:55 -0300

    Progresso documentacao

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>AWS MlOps pipeline</span>,
+  useNextSeoProps() {
+    return { titleTemplate: 'MlOps Pipeline - %s' }
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/RicardoRibeiroRodrigues/AWS-IaC-mlops-pipeline',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/RicardoRibeiroRodrigues/aws-iaC-mlops-pipeline-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Documentação do projeto',
   },
 }
 
